6125c07fcca4499f13d6f12d0fc4951c4819720c
Laura Darlak <laura@polyporedesign.com>
2018-09-19 19:07:11 +0000

    Create Beer class to save scraped Beer instances

---
+++ lib/OnTapIBC/beer.rb
@@ -0,0 +1,12 @@
+class OnTapIBC::Beer
+  attr_accessor :name, :abv, :short_desc, :url
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.save
+    @@all << self
+end
--- lib/OnTapIBC/tap_list.rb
+++ lib/OnTapIBC/tap_list.rb
@@ -29,16 +29,15 @@ class OnTapIBC::Taps
 
   def self.scrape_menu
     doc = Nokogiri::HTML(open("https://www.ithacabeer.com/taproom-menu/#anchor-tap-list"))
-    beer_list = []
     beers = doc.search("div#block-yui_3_17_2_8_1508769314339_15193 div.menu-items div.menu-item")
     beers.each do |beer|
-      new_beer = OnTapIBC::Taps.new # create beer instance
+      new_beer = OnTapIBC::Beer.new # create beer instance
       new_beer.name = beer.css("div.menu-item-title").text.strip # assign beer name
       new_beer.abv = beer.css("span.menu-item-price-top").text.strip # assign ABV
       new_beer.short_desc = beer.css("div.menu-item-description").text.strip # assign short description
-      beer_list << new_beer
+      new_beer.save
     end
-    beer_list
+
   end
 
 end
